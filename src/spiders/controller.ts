7a504d46341b54c257f96bd19457c673b5597dc6
zsx <mrzsxofanhui@163.com>
2024-04-28 18:06:34 +0800

    1

--- src/spiders/controller.ts
+++ src/spiders/controller.ts
@@ -1,10 +1,19 @@
 import { Express } from "express";
-import { JSONFilePreset } from 'lowdb/node'
+import { JSONFilePreset } from "lowdb/node";
+import { xiaobaiPath } from "../config.ts";
+import path from "node:path";
 
 function createSpiderRules(app: Express) {
-    app.post('/spiders/rule', async (req, res) => {
-        // const db = await JSONFilePreset()
-    })
+  app.post("/spiders/rule", async (req, res) => {
+    // const db = await JSONFilePreset()
+  });
+}
+
+function querySpiderRules(app: Express) {
+  app.get("/spiders/rule", async (req, res) => {
+    const db = await JSONFilePreset(path.resolve(xiaobaiPath, "./db.json"), []);
+    
+  });
 }
 
 export { createSpiderRules };
